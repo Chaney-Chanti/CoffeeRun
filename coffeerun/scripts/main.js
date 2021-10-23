774d4f951022f6a8384f3cbe34a97d978d666914
Chaney Chantipaporn <66577982+ChaneyChanti@users.noreply.github.com>
2021-10-22 23:11:23 -0700

    Add files via upload

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -2,39 +2,27 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    // var VALIDATE_SELECTOR = '[data-validate="clientcheck"]';
-    // var SERVER_URL = 'http://co.audstanley.com/coffeeorders';
-    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    // var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    var FIREBASE_SERVER_URL = 'http://';
 
     var App = window.App;
     var Truck = App.Truck;
-    var DataStore = App.DataStore;
-    var RemoteDataStore = App.RemoteDataStore;
+    // var DataStore = App.DataStore;
+    // var RemoteDataStore = App.RemoteDataStore;
+    var firebasedatastore = App.firebasedatastore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
 
-    var remoteDS = new RemoteDataStore(SERVER_URL);
-    var datastore = new DataStore();
-    var truck = new Truck('ncc-1705', remoteDS);
-    // var truck = new Truck('ncc-1705', datastore);
+    var datastore = new firebasedatastore(FIREBASE_SERVER_URL);
+    var truck = new Truck('ncc-1705', datastore);
     window.truck = truck;
 
     var checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(truck.deliverOrder.bind(truck));
 
-    // var formHandler = new FormHandler(VALIDATE_SELECTOR);
-    // formHandler.addValidateHandler(function(data) {
-    //     console.log(data)
-    //     return truck.createOrder.call(truck, data)
-    //     .then(function() {
-    //         checkList.addRow.call(checkList, data);
-    //     },
-    //     function() { 
-    //         alert('Server unreachable. Try again later.');
-    //     }); 
-    // });
-
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(truck.deliverOrder.bind(truck));
     var formHandler = new FormHandler(FORM_SELECTOR);
     formHandler.addSubmitHandler(function(data) {
         return truck.createOrder.call(truck, data)
@@ -43,12 +31,10 @@
             },
             function() { 
                 alert('Server unreachable. Try again later.');
-            }); 
+            });
     });
 
-
-    // formHandler.addInputHandler(Validation.isCompanyEmail);
-    // formHandler2.addInputHandler(Validation.isCompanyEmail);
+    formHandler.addInputHandler(Validation.isCompanyEmail);
 
     truck.printOrders(checkList.addRow.bind(checkList));
 
